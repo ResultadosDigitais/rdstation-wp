427bbe04f3377773847a18b0ae1f02d8da5d56dc
Filipe Nascimento <filipemateusn@gmail.com>
2017-02-17 17:36:17 -0200

    add new line at the end of files

--- settings/settings_menu.php
+++ settings/settings_menu.php
@@ -4,7 +4,7 @@ add_action( 'admin_menu', 'rdstation_menu' );
 function rdstation_menu() {
   add_options_page(
     'Configurações RD Station',
-    'RD Station',
+    'Integração RD Station',
     'manage_options',
     'rdstation-settings-page',
     'rdstation_settings_page_callback'
