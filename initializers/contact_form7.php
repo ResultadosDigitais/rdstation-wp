946e440b672ba057a2470e5de243f66a024ccd39
Filipe Nascimento <filipemateusn@gmail.com>
2017-02-22 17:36:38 -0300

    move custom post type calling to initializers

--- initializers/contact_form7.php
+++ initializers/contact_form7.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('RD_Custom_Post_Type.php');
+
 $setup = new RD_Custom_Post_Type('rdcf7');
 $setup->acronym = 'CF7';
 $setup->name = 'Contact Form 7';
--- initializers/gravity_forms.php
+++ initializers/gravity_forms.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('RD_Custom_Post_Type.php');
+
 $setup = new RD_Custom_Post_Type('rdgf');
 $setup->acronym = 'GF';
 $setup->name = 'Gravity Forms';
