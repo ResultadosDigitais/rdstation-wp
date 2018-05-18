48daa242894307cebe21f4badfa503b808219e9c
Filipe Nascimento <filipemateusn@gmail.com>
2018-05-18 14:00:24 -0300

    Fix missing semicolon :sweat:

--- config.php
+++ config.php
@@ -4,4 +4,4 @@ define('LEGACY_API_URL', 'http://app.rdstation.com.br/api/1.3');
 define('API_URL', 'https://api.rd.services');
 
 define('CONVERSIONS', '/conversions');
-define('CONTACTS', '/platform/contacts/')
+define('CONTACTS', '/platform/contacts/');
