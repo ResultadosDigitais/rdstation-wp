b300f133ff1dd24192b30eb325377a61e057cd34
Filipe Nascimento <filipemateusn@gmail.com>
2018-05-21 15:00:04 -0300

    Change client id

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,6 +1,6 @@
 (function RDStationIntegration() {
   var SERVER_ORIGIN = 'https://je5ypxtc6b.execute-api.us-west-2.amazonaws.com';
-  var CLIENT_ID = '12051950-222a-4513-bf02-638364768099';
+  var CLIENT_ID = 'c9d14ec8-2671-404e-b337-ebae63906a8b';
   var REDIRECT_URL = 'https://je5ypxtc6b.execute-api.us-west-2.amazonaws.com/dev/oauth/callback';
   var LEGACY_TOKENS_ENDPOINT = 'https://api.rd.services/platform/legacy/tokens';
   var newWindowInstance = null;
