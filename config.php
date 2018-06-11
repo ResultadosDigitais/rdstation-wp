388434500659486c21b1328c0f3ff555359202ad
Filipe Nascimento <filipemateusn@gmail.com>
2018-06-11 14:30:09 -0300

    Change refresh token URL

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('RDSM_SRC_DIR', dirname(__FILE__) . '/includes');
 // URIs
 define('LEGACY_API_URL', 'http://app.rdstation.com.br/api/1.3');
 define('API_URL', 'https://api.rd.services');
-define('REFRESH_TOKEN_URL', 'https://je5ypxtc6b.execute-api.us-west-2.amazonaws.com/dev/oauth/refresh');
+define('REFRESH_TOKEN_URL', 'https://wp.rd.services/prod/oauth/refresh');
 
 // Endpoints
 define('CONVERSIONS', '/conversions');
