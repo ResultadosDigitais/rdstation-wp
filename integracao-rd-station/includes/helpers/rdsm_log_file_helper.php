85e5d47ec49449515f1968ad1af00c648abe1440
Anderson Oliveira <andersonoliveira@MacBook-Pro-de-Anderson.local>
2021-02-23 11:50:06 -0300

    add time zone to date format

--- integracao-rd-station/includes/helpers/rdsm_log_file_helper.php
+++ integracao-rd-station/includes/helpers/rdsm_log_file_helper.php
@@ -3,7 +3,7 @@
 class RDSMLogFileHelper {
 
   public static function write_to_log_file($value) {
-    $time = date( "F jS Y, H:i", time()-10800 );
+    $time = date( "F jS Y, H:i P", time() );
     $log = "#$time\r\n$value\r\n";
     $file = RDSM_LOG_FILE_PATH;
     $open = fopen( $file, "a" );
